fb03447c6a3a04f2a26b6771342fc7a219d6fde0
Antonio Barber <antonio.barber@wsu.edu>
2021-05-16 23:09:03 -0700

    added more mentioanbles

--- src/mention/transforms/insertMention.ts
+++ src/mention/transforms/insertMention.ts
@@ -2,6 +2,11 @@ import { Editor, Transforms } from 'slate';
 import { setDefaults } from '@udecode/slate-plugins';
 import { DEFAULTS_MENTION } from '../defaults';
 import { MentionNode, MentionNodeData, MentionPluginOptions } from '../types';
+import { getEmptyFractionNode } from '../../Components/Fraction/thfraction/getEmptyFractionNode'
+import { getEmptySummationNode } from '../../Components/Summation/getEmptySummationNode';
+import { getEmptyBigOpNode } from '../../Components/BigOperator/getEmptyBigOpNode'
+import {getEmptyIntegralNode} from '../../Components/Integral/getEmptyIntegralNode'
+import {getEmptyLimNode} from '../../Components/Limit/getEmptyLimNode'
 
 export const insertMention = (
   editor: Editor,
@@ -15,6 +20,29 @@ export const insertMention = (
     children: [{ text: '' }],
     ...mentionable,
   };
-  Transforms.insertNodes(editor, mentionNode);
-  Transforms.move(editor);
+
+  if (mentionNode.test.match("integralcomponent")) {
+    Transforms.insertNodes(editor, getEmptyIntegralNode());
+    Transforms.move(editor);
+  }
+  else if (mentionNode.test.match("fraction")){
+    Transforms.insertNodes(editor, getEmptyFractionNode());
+    Transforms.move(editor);
+  }
+  else if (mentionNode.test.match("sumcomponent")){
+    Transforms.insertNodes(editor, getEmptySummationNode());
+    Transforms.move(editor);
+  }
+  else if (mentionNode.test.match("limcomponent")){
+    Transforms.insertNodes(editor, getEmptyLimNode());
+    Transforms.move(editor);
+  }
+  else if (mentionNode.test.match("bigoperator")){
+    Transforms.insertNodes(editor, getEmptyBigOpNode());
+    Transforms.move(editor);
+  }
+  else {
+    Transforms.insertNodes(editor, mentionNode);
+    Transforms.move(editor);
+  }
 };
